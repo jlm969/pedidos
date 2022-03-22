d99d822faea83f89357b2188088dc2353bd0fcf3
Jorge Marquez <jlm969969@gmail.com>
2022-03-22 20:06:33 -0300

    Se registraron las tablas, Carga de datos por panel Django

--- apppedido/admin.py
+++ apppedido/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from apppedido.models import Cliente, Producto, Pedido, Repartidor
 
 # Register your models here.
+admin.site.register(Cliente)
+admin.site.register(Producto)
+admin.site.register(Pedido)
+admin.site.register(Repartidor)
--- apppedido/models.py
+++ apppedido/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Cliente(models.Model):
-    num_celular = models.IntegerField(primary_key=True)
+    num_celular = models.CharField(max_length=15, primary_key=True)
     nombre = models.CharField(max_length=40)
     apellido = models.CharField(max_length=40)
     direccion = models.CharField(max_length=40)
@@ -13,28 +13,31 @@ class Cliente(models.Model):
 class Producto(models.Model):
     id_producto = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=40)
-    precio = models.FloatField()    
+    precio = models.FloatField()  
+
+class Repartidor(models.Model):
+    id_repartidor = models.CharField(max_length=4 ,primary_key=True)
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    direccion = models.CharField(max_length=40)
+    email = models.EmailField()
+    num_celular = models.CharField(max_length=40)
+    sexos = [
+        ("F", "Femenino"), 
+        ("M", "Masculino"), 
+    ]
+    sexo = models.CharField(max_length=1, choices=sexos, default="F")
+    
+
+    def nombreCompleto(self):
+        txt = "{0} {1}"
+        return txt.format(self.apellido, self.nombre)
 
 class Pedido(models.Model):
     id_pedido = models.AutoField(primary_key=True)
     producto = models.ForeignKey(Producto, null=False, blank=False, on_delete=models.CASCADE)
     cliente = models.ForeignKey(Cliente, null=False, blank=False, on_delete=models.CASCADE)
     fecha = models.DateTimeField(auto_now_add=True)
+    repartidor = models.ForeignKey(Repartidor, null=True, blank=True, on_delete=models.CASCADE)
 
 
-class Repartidor(models.Model):
-    id_repartdir = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=40)
-    apellido = models.CharField(max_length=40)
-    direccion = models.CharField(max_length=40)
-    email = models.EmailField()
-    num_celular = models.IntegerField()
-    sexos = [
-        ("F", "Femenino"), 
-        ("M", "Masculino"), 
-    ]
-    sexo = models.CharField(max_length=1, choices=sexos, default="F")
-    pedido = models.ForeignKey(Pedido, null=False, blank=False, on_delete=models.CASCADE)
-
-    def nombreCompleto(self):
-        return (self.apellido, self.nombre)
